61ba150852a5c6eff4a2a8933d792a29d70cc399
gaiyadev <gaiyaobed94@gmail.com>
2021-07-13 20:37:11 +0100

    count user daa

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -8,6 +8,8 @@ import PeopleIcon from "@material-ui/icons/People";
 import BugReportIcon from "@material-ui/icons/BugReport";
 import MergeTypeIcon from "@material-ui/icons/MergeType";
 import AccountBalanceIcon from "@material-ui/icons/AccountBalance";
+const baseUrl = "http://localhost:5000/api/v1/cars";
+const baseUrl2 = "http://localhost:5000/api/v1/users";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -20,7 +22,19 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const Home = () => {
+export const getStaticProps = async () => {
+  const res = await fetch(`${baseUrl}/count`);
+  const res2 = await fetch(`${baseUrl2}/count`);
+  const data = await res.json();
+  const data2 = await res2.json();
+  const carsList = data.cars;
+  const carsList2 = data2.users;
+  return {
+    props: { cars: carsList, users: carsList2 },
+  };
+};
+
+const Home = ({ cars, users }) => {
   const classes = useStyles();
   const dispatch = useDispatch();
   const [isLoading, setIsLoading] = useState(false);
@@ -43,10 +57,10 @@ const Home = () => {
     <div className={classes.root}>
       <Grid container spacing={3}>
         <Grid item xs={12} md={3} sm={12}>
-          <ScoreCard title="Faults" stats="5" icon={<BugReportIcon />} />
+          <ScoreCard title="Faults" stats={cars} icon={<BugReportIcon />} />
         </Grid>
         <Grid item xs={12} md={3} sm={12}>
-          <ScoreCard title="Users" stats="85" icon={<PeopleIcon />} />
+          <ScoreCard title="Users" stats={users} icon={<PeopleIcon />} />
         </Grid>
         <Grid item xs={12} md={3} sm={12}>
           <ScoreCard
